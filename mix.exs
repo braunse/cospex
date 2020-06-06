76c13ad3888f07e515b72117fe9ac29bfa758f2b
Sebastien Braun <sebastien@sebbraun.de>
2020-06-06 16:30:25 +0200

    Add license metadata

--- mix.exs
+++ mix.exs
@@ -4,6 +4,7 @@ defmodule Cospex.MixProject do
   def project do
     [
       app: :cospex,
+      licenses: ["MPL-2.0"],
       version: "0.1.0",
       elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
